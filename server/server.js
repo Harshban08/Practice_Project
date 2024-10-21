266ff5ef76a7f157cdad10ab0e7055ffb91aea8a
Harsh_Bansal <harshbansal49177@gmail.com>
2024-10-21 11:46:41 +0530

    Commit 1

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("TEST")
+const express = require('express');
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware.errHandler");
+const cors = require("cors")
+
+connectDb();
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Working');
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http:localhost:${PORT}`);
+});
